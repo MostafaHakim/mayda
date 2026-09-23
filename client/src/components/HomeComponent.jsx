3aa7aed8f75477f006ddd272ecb3a52d6e739ede
MostafaHakim <mdmostafahakim@gmail.com>
2026-09-23 11:32:08 -0700

    new design

--- client/src/components/HomeComponent.jsx
+++ client/src/components/HomeComponent.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import SliderComponent from "./SliderComponent";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
 const HomeComponent = () => {
   const foods = [
     {
@@ -103,6 +107,231 @@ const HomeComponent = () => {
             Fresh ingredients, delicious taste and amazing food made specially
             for you.
           </p>
+        </div>
+
+        <div>
+          <div>
+            <h2 className="text-xl font-bold md:text-4xl text-[#690303]">
+              Halal
+            </h2>
+            <p className="text-sm md:text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+              debitis.
+            </p>
+          </div>
+          <div className=" py-4">
+            <Swiper
+              spaceBetween={6}
+              slidesPerView={2.2}
+              breakpoints={{
+                480: {
+                  slidesPerView: 2.5,
+                  spaceBetween: 8,
+                },
+                768: {
+                  slidesPerView: 3.2,
+                  spaceBetween: 10,
+                },
+                1024: {
+                  slidesPerView: 4.2,
+                  spaceBetween: 10,
+                },
+              }}
+              onSlideChange={() => console.log("slide change")}
+              onSwiper={(swiper) => console.log(swiper)}
+              className="w-full h-full rounded-xl overflow-hidden"
+            >
+              {foods.map((food) => (
+                <SwiperSlide className="w-full h-full relative ">
+                  <div
+                    key={food.id}
+                    className="group bg-white rounded-2xl overflow-hidden
+        shadow-md hover:shadow-2xl
+        border border-gray-100
+        transition-all duration-300
+        hover:-translate-y-2"
+                  >
+                    {/* Image */}
+                    <div className="relative overflow-hidden h-52">
+                      <img
+                        src={food.image}
+                        alt={food.name}
+                        className="w-full h-full object-cover
+            group-hover:scale-110
+            transition-transform duration-500"
+                      />
+
+                      {/* Price Badge */}
+                      <div
+                        className="absolute top-3 right-3
+            bg-white px-3 py-1 rounded-full
+            shadow-md font-bold text-[#690303]"
+                      >
+                        ${food.price}
+                      </div>
+                    </div>
+
+                    {/* Content */}
+                    <div className="p-1 md:p-5">
+                      <h2
+                        className="text-sm md:text-xl font-bold text-gray-800
+            group-hover:text-[#690303]
+            transition-colors duration-300"
+                      >
+                        {food.name}
+                      </h2>
+
+                      <p className="text-gray-500 text-xs md:text-sm md:mt-2 leading-6 line-clamp-2">
+                        {food.description}
+                      </p>
+
+                      {/* Buttons */}
+                      <div className="flex flex-col sm:flex-row gap-3 mt-5">
+                        <button
+                          className="flex-1 bg-[#690303]
+                    cursor-pointer
+              hover:bg-[#690303]
+              text-white font-semibold
+              py-2.5 rounded-lg
+              transition duration-300
+              active:scale-95"
+                        >
+                          Order Now
+                        </button>
+
+                        <button
+                          className="px-4 py-2.5
+                    cursor-pointer
+              border border-[#690303]
+              text-[#690303]
+              hover:bg-[#690303]
+              hover:text-white
+              font-semibold
+              rounded-lg
+              transition duration-300
+              active:scale-95"
+                        >
+                          See more
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2 className="text-xl font-bold md:text-4xl text-[#690303]">
+              Non-Halal
+            </h2>
+            <p className="text-sm md:text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+              debitis.
+            </p>
+          </div>
+          <div className=" py-4">
+            <Swiper
+              spaceBetween={6}
+              slidesPerView={2.2}
+              breakpoints={{
+                480: {
+                  slidesPerView: 2.5,
+                  spaceBetween: 8,
+                },
+                768: {
+                  slidesPerView: 3.2,
+                  spaceBetween: 10,
+                },
+                1024: {
+                  slidesPerView: 4.2,
+                  spaceBetween: 10,
+                },
+              }}
+              onSlideChange={() => console.log("slide change")}
+              onSwiper={(swiper) => console.log(swiper)}
+              className="w-full h-full rounded-xl overflow-hidden"
+            >
+              {foods.map((food) => (
+                <SwiperSlide className="w-full h-full relative ">
+                  <div
+                    key={food.id}
+                    className="group bg-white rounded-2xl overflow-hidden
+        shadow-md hover:shadow-2xl
+        border border-gray-100
+        transition-all duration-300
+        hover:-translate-y-2"
+                  >
+                    {/* Image */}
+                    <div className="relative overflow-hidden h-52">
+                      <img
+                        src={food.image}
+                        alt={food.name}
+                        className="w-full h-full object-cover
+            group-hover:scale-110
+            transition-transform duration-500"
+                      />
+
+                      {/* Price Badge */}
+                      <div
+                        className="absolute top-3 right-3
+            bg-white px-3 py-1 rounded-full
+            shadow-md font-bold text-[#690303]"
+                      >
+                        ${food.price}
+                      </div>
+                    </div>
+
+                    {/* Content */}
+                    <div className="p-1 md:p-5">
+                      <h2
+                        className="text-sm md:text-xl font-bold text-gray-800
+            group-hover:text-[#690303]
+            transition-colors duration-300"
+                      >
+                        {food.name}
+                      </h2>
+
+                      <p className="text-gray-500 text-xs md:text-sm md:mt-2 leading-6 line-clamp-2">
+                        {food.description}
+                      </p>
+
+                      {/* Buttons */}
+                      <div className="flex flex-col sm:flex-row gap-3 mt-5">
+                        <button
+                          className="flex-1 bg-[#690303]
+                    cursor-pointer
+              hover:bg-[#690303]
+              text-white font-semibold
+              py-2.5 rounded-lg
+              transition duration-300
+              active:scale-95"
+                        >
+                          Order Now
+                        </button>
+
+                        <button
+                          className="px-4 py-2.5
+                    cursor-pointer
+              border border-[#690303]
+              text-[#690303]
+              hover:bg-[#690303]
+              hover:text-white
+              font-semibold
+              rounded-lg
+              transition duration-300
+              active:scale-95"
+                        >
+                          See more
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </div>
 
         {/* Food Cards */}
